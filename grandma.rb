a6fdaf40a1026f23455e7572222efe5a9a855bc7
chris <cseiter@gmail.com>
2021-07-05 09:33:47 -0400

    lets golf it using case/switch and regex

--- grandma.rb
+++ grandma.rb
@@ -12,13 +12,21 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(phrase)
-    if phrase == "I LOVE YOU GRANDMA!"
-        then response = "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase.upcase == phrase
-        then response = "NO, NOT SINCE 1938!"
-    else response = "HUH?! SPEAK UP, SONNY!"
+def speak_to_grandma(p)
+    # if phrase == "I LOVE YOU GRANDMA!"
+    #     then response = "I LOVE YOU TOO PUMPKIN!"
+    # elsif phrase.upcase == phrase
+    #     then response = "NO, NOT SINCE 1938!"
+    # else response = "HUH?! SPEAK UP, SONNY!"
+    # end
+    # puts response
+    # return response
+    case p
+    when "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+    when /[a-z]/
+    "HUH?! SPEAK UP, SONNY!"
+    else
+    "NO, NOT SINCE 1938!"
     end
-    puts response
-    return response
 end
