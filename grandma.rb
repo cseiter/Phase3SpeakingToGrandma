44acdfa30b77fcc0409ccfd50c76f7ac92ac56b5
chris <cseiter@gmail.com>
2021-07-05 09:08:21 -0400

    template method created

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,9 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(phrase)
+    puts phrase
+end
+
+speak_to_grandma("test phrase")
